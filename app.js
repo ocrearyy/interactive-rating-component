147b91e1b2bb5e58c14cd4f4d54128ac642b2d79
oshanedesign <oshane.design@gmail.com>
2022-04-18 21:52:34 -0500

    thank you popup

---
+++ app.js
@@ -0,0 +1,13 @@
+function displaypopup() {
+    document.querySelector('#popup').style.display = 'block';
+    document.querySelector('#rating-section').style.display = 'none';
+
+
+    }
+
+
+
+// const popup = document.querySelector('#popup');
+// popup.style.display = 'block';
+
+// popup.addEventListener('click', displaypopup());
