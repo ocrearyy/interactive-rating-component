aff39b0f328cca94447754b6513c1e7891434609
oshanedesign <oshane.design@gmail.com>
2022-04-20 23:20:48 -0500

    select a rating

--- app.js
+++ app.js
@@ -3,9 +3,42 @@ function displaypopup() {
     document.querySelector('#rating-section').style.display = 'none';
 }
 
+function rating1(){
+    document.querySelector('#one').style.backgroundColor = 'hsl(25, 97%, 53%)';
+    document.querySelector('#two').style.backgroundColor = '#273039';
+    document.querySelector('#three').style.backgroundColor = '#273039';
+    document.querySelector('#four').style.backgroundColor = '#273039';
+    document.querySelector('#five').style.backgroundColor = '#273039';
+}
 
-const rating1 = document.querySelector('#one');
-const rating2 = document.querySelector('#two');
-const rating3 = document.querySelector('#three');
-const rating4 = document.querySelector('#four');
-const rating5 = document.querySelector('#five');
+function rating2(){
+    document.querySelector('#one').style.backgroundColor = '#273039';
+    document.querySelector('#two').style.backgroundColor = 'hsl(25, 97%, 53%)';
+    document.querySelector('#three').style.backgroundColor = '#273039';
+    document.querySelector('#four').style.backgroundColor = '#273039';
+    document.querySelector('#five').style.backgroundColor = '#273039';
+}
+
+function rating3(){
+    document.querySelector('#one').style.backgroundColor = '#273039';
+    document.querySelector('#two').style.backgroundColor = '#273039';
+    document.querySelector('#three').style.backgroundColor = 'hsl(25, 97%, 53%)';
+    document.querySelector('#four').style.backgroundColor = '#273039';
+    document.querySelector('#five').style.backgroundColor = '#273039';
+}
+
+function rating4(){
+    document.querySelector('#one').style.backgroundColor = '#273039';
+    document.querySelector('#two').style.backgroundColor = '#273039';
+    document.querySelector('#three').style.backgroundColor = ' #273039';
+    document.querySelector('#four').style.backgroundColor = 'hsl(25, 97%, 53%)';
+    document.querySelector('#five').style.backgroundColor = '#273039';
+}
+
+function rating5(){
+    document.querySelector('#one').style.backgroundColor = '#273039';
+    document.querySelector('#two').style.backgroundColor = '#273039';
+    document.querySelector('#three').style.backgroundColor = ' #273039';
+    document.querySelector('#four').style.backgroundColor = '#273039';
+    document.querySelector('#five').style.backgroundColor = 'hsl(25, 97%, 53%)';
+}
